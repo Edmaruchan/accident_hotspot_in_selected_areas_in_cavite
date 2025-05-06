7ff5bcb27893baa6a07ae68dd516a3aee3f914a2
Edmaruchan <136604382+Edmaruchan@users.noreply.github.com>
2025-05-06 13:26:07 +0000

    added bar and line graph

--- pages/GMA.py
+++ pages/GMA.py
@@ -27,11 +27,26 @@ stats_view = st.radio("View Statistics By", ["Year", "Month"])
 if stats_view == "Year":
     st.subheader("Total Incidents per Year")
     yearly_counts = df['Year'].value_counts().sort_index()
-    st.bar_chart(yearly_counts)
+    st.line_chart(yearly_counts)
     st.write("The year 2023 has the highest number of incidents.")
     st.write("The year 2020 has the lowest number of incidents.")
 
+    st.subheader("Total Incidents per Year")
+    yearly_counts = df['Year'].value_counts().sort_index()
+    st.bar_chart(yearly_counts)
+    st.write("The year 2023 has the highest number of incidents.")
+    st.write("The year 2020 has the lowest number of incidents.")
+    
+
 elif stats_view == "Month":
+    st.subheader("Total Incidents per Month")
+    month_order = ["January", "February", "March", "April", "May", "June",
+                   "July", "August", "September", "October", "November", "December"]
+    monthly_counts = df['Month'].value_counts().reindex(month_order)
+    st.line_chart(monthly_counts)
+    st.write("June and September have the highest number of incidents.")
+    st.write("August have the lowest number of incidents.")
+
     st.subheader("Total Incidents per Month")
     month_order = ["January", "February", "March", "April", "May", "June",
                    "July", "August", "September", "October", "November", "December"]
